99e6b7b99a6ae31ccc9e657c774a6d63c67737d7
selim-haspolat <selimhaspolat@gmail.com>
2023-01-03 17:59:56 +0300

    12-Countingsheep...  solved

---
+++ 12-Counting sheep/app.js
@@ -0,0 +1,26 @@
+//? Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+//? For example,
+//? [true,  true,  true,  false,
+//?   true,  true,  true,  true ,
+//?   true,  false, true,  false,
+//?   true,  false, false, true ,
+//?   true,  true,  true,  true ,
+//?   false, false, true,  true]
+//? The correct answer would be 17.
+//? Hint: Don't forget to check for bad values like null/undefined
+
+const countSheeps = (arrayOfSheeps) =>{
+    counter = 0
+    for(i = 0; i< arrayOfSheeps.length; i++){
+        arrayOfSheeps[i] === true ? counter++: counter;
+    } 
+    return counter
+}
+
+let arr = [true,  true,  true,  false,
+    true,  true,  true,  true ,
+    true,  false, true,  false,
+    true,  false, false, true ,
+    true,  true,  true,  true ,
+    false, false, true,  true]
+countSheeps(arr)
